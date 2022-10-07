4594348e95852d90e48712c09f92582f957b8764
wayofthepie <wayofthepie@gmail.com>
2022-10-07 22:11:00 +0100

    Remove tty flag

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 image_name=${1:-packer-build}
 docker build --tag ${image_name} .
-docker run -ti -v ${PWD}:/var/tmp ${image_name} /var/tmp/test.sh
+docker run -i -v ${PWD}:/var/tmp ${image_name} /var/tmp/test.sh
